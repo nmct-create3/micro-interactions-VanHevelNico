eb0b004788922ee402391dc735768089307d10e0
VanHevelNico <51059474+VanHevelNico@users.noreply.github.com>
2020-10-09 22:23:31 +0200

    Dark theme

--- script/app.js
+++ script/app.js
@@ -3,7 +3,20 @@ function handleFloatingLabel() {
 }
 
 function handlePasswordSwitcher() {
-    
+    btn = document.getElementsByClassName("js-password-toggle");
+    for (var i = 0 ; i < btn.length; i++) {
+        btn[i].addEventListener('change' , changePass, btn[i] ) ; 
+     }     
+}
+
+function changePass () {
+    console.log('ik werk');
+    input = document.getElementById("password");
+    if(this.checked) {
+        input.type = 'text';
+    } else {
+        input.type = 'password';
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
